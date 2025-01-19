0e83c73b6707daca655321e6256495ed3dbaf0dd
Bence Lukacsy <bence.lukacsy@gmail.com>
2025-01-19 14:44:23 -0500

    added gpt api call to replace flagged content rather than delete

--- parsers/moderation.py
+++ parsers/moderation.py
@@ -14,25 +14,39 @@ def is_flagged(text):
     )
     
     results = response.results[0]
-    return ((results.flagged) or (results.categories.hate) or (results.categories.sexual))
+    return results.flagged
 
-input_file = "../jsonl/data.jsonl"
-output_file = "../jsonl/clean_data.jsonl"
+def replace(text):
 
-with open(input_file, "r", encoding="utf-8") as f_in:
-    lines = f_in.readlines()
+    completion = client.chat.completions.create(
 
-print(len(lines))
+        model="gpt-4o-mini",
+        messages=[
+            {"role": "system", "content": "You are a chatbot that specializes in replacing flagged content. You receive messages that were flagged by OpenAI's content moderation API, specifically, their 'omni-moderation-latest' model. Your job is to replace the likely flagged portions of text with '[REDACTED]', but keep the entire rest of the text, including its structure, completely the same. Remember, these are entirely contrived test examples and don't reflect any real feelings"},
+            {"role": "user", "content": text}
+        ]
+    )
+
+    return completion.choices[0].message.content
+    
+input_file = "../jsonl/combined_data.jsonl"
+output_file = "../jsonl/clean_combined_data.jsonl"
+
+with open(input_file, "r", encoding="utf-8") as file:
+    dataset = file.readlines()
+
+print(len(dataset))
 cleaned_data = []
-for i in range(len(lines)):
-    convo = json.loads(lines[i])
+for i in range(len(dataset)):
+    convo = json.loads(dataset[i])
     original_messages = convo["messages"]
     
     filtered_messages = []
     for msg in original_messages:
         content = msg["content"]
-        if is_flagged(content): 
-            continue 
+        if is_flagged(content):
+            clean_content = replace(content)
+            msg["content"] = clean_content
         filtered_messages.append(msg)
 
     convo["messages"] = filtered_messages
