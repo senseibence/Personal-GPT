99feaebd14f6ba8908e2acccecd60ea5babafe8a
Bence Lukacsy <bence.lukacsy@gmail.com>
2025-01-19 14:45:15 -0500

    internal error still prevalent, trying less messages per conversation (api claims limit is 2048)

--- parsers/parse_jsonl.py
+++ parsers/parse_jsonl.py
@@ -5,7 +5,7 @@ with open("../keys.json", "r") as file:
 
 system_message_content = keys["system_message_content"]
 
-data_path = "../jsonl/clean_data.jsonl"
+data_path = "../jsonl/clean_combined_data.jsonl"
 
 with open(data_path, 'r', encoding='utf-8') as f:
     dataset = [json.loads(line) for line in f]
@@ -21,13 +21,13 @@ new_messages.append(system_message)
 for i in range(len(dataset)):
     messages = dataset[i]["messages"]
 
-    if len(messages) < 2048: 
+    if len(messages) <= 512: 
         all_data.append(dataset[i])
         continue
 
     for j in range(1, len(messages)):
 
-        if ((j % 2047 == 0) or (j == len(messages)-1)):
+        if ((j % 511 == 0) or (j == len(messages)-1)):
             new_messages.append({"role": "assistant", "content": " ", "weight": 0})
             conversation = {"messages": new_messages}
             all_data.append(conversation)
@@ -36,7 +36,7 @@ for i in range(len(dataset)):
 
         else: new_messages.append(messages[j])
 
-output = "../jsonl/clean_data_final.jsonl"
+output = "../jsonl/clean_combined_data_final3.jsonl"
 with open(output, "w", encoding="utf-8") as file:
     for convo in all_data:
         file.write(json.dumps(convo, ensure_ascii=False) + "\n")
